8bc71d4f2d2452c14400430ff3eba7e0f6323801
Terminator_97 <50580453+francesco132@users.noreply.github.com>
2020-08-24 00:45:16 +0200

    2.3.1

--- SCPUtils/Plugin.cs
+++ SCPUtils/Plugin.cs
@@ -17,8 +17,8 @@ namespace SCPUtils
         public static string pluginVersion = "2.3.1";
         public override string Author { get; } = "Terminator_9#0507";
         public override string Name { get; } = "SCPUtils";
-        public override Version Version { get; } = new Version(2, 3, 0);
-        public override Version RequiredExiledVersion { get; } = new Version(2, 0, 10);
+        public override Version Version { get; } = new Version(2, 3, 1);
+        public override Version RequiredExiledVersion { get; } = new Version(2, 1, 2);
         public EventHandlers EventHandlers { get; private set; }
         public Functions Functions { get; private set; }
         public Player Player { get; private set; }
