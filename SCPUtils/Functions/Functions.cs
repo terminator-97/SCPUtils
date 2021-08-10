57627352084d3463eda56516bfa50c5a5f3f4245
Terminator_97 <50580453+francesco132@users.noreply.github.com>
2021-08-10 22:14:39 +0200

    2.7.4

--- SCPUtils/Functions/Functions.cs
+++ SCPUtils/Functions/Functions.cs
@@ -322,8 +322,9 @@ namespace SCPUtils
                 if (pluginInstance.Config.BroadcastSanctions)
                 {
                     if (admin.ReferenceHub.serverRoles.RemoteAdmin)
+                    if (admin.Sender.CheckPermission(PlayerPermissions.AdminChat))
                     {
-                        admin.Broadcast(12, text, Broadcast.BroadcastFlags.AdminChat);
+                            admin.Broadcast(12, text, Broadcast.BroadcastFlags.AdminChat);
                     }
                 }
             }
